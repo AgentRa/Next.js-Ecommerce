8bc4795cbabdec9e15ae21884da052809f26569a
agentRa <bubonik.san1993@gmail.com>
2024-12-29 16:34:09 +0100

    Feat: add shadcn button component, (root) layout, metadata information

---
+++ ecommerce-app/lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "eProstore";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern e commerce application build with next 15 and react 19"
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
