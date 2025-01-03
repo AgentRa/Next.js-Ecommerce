28160b0d4d1a15f107789403ac58b53c0a8d47ae
agentRa <bubonik.san1993@gmail.com>
2025-01-03 13:04:25 +0100

    Feat: add product list component, with sample data set

--- ecommerce-app/app/(root)/page.tsx
+++ ecommerce-app/app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
   return (
-    <>
-      <>Latest Products</>
-    </>
+    <div className="my-10">
+        <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+    </div>
   );
 };
 
