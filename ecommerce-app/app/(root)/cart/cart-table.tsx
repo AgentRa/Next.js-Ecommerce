7fac845069eda992903ddd2783785a03c90d27e1
agentRa <bubonik.san1993@gmail.com>
2025-01-07 18:24:39 +0100

    Feat: add cart Table summary card

--- ecommerce-app/app/(root)/cart/cart-table.tsx
+++ ecommerce-app/app/(root)/cart/cart-table.tsx
@@ -16,6 +16,8 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/lib/utils";
+import { Card, CardContent } from "@/components/ui/card";
 
 type Props = { cart?: Cart };
 export default function CartTable({ cart }: Props) {
@@ -87,40 +89,59 @@ export default function CartTable({ cart }: Props) {
                       </Button>
                       <span>{item.qty}</span>
                       <Button
-                          className=""
-                          disabled={isPending}
-                          variant="outline"
-                          type="button"
-                          onClick={() =>
-                              startTransition(async () => {
-                                const res = await addItemToCart(
-                                    item,
-                                );
+                        className=""
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
 
-                                if (!res.success) {
-                                  toast({
-                                    variant: "destructive",
-                                    description: res.message,
-                                  });
-                                }
-                              })
-                          }
+                            if (!res.success) {
+                              toast({
+                                variant: "destructive",
+                                description: res.message,
+                              });
+                            }
+                          })
+                        }
                       >
                         {isPending ? (
-                            <Loader className="h-4 w-4 animate-spin" />
+                          <Loader className="h-4 w-4 animate-spin" />
                         ) : (
-                            <Plus className="w-4 h-4" />
+                          <Plus className="w-4 h-4" />
                         )}
                       </Button>
                     </TableCell>
-                    <TableCell className="text-right">
-                      ${(item.price)}
-                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </div>
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                <span className="font-bold">
+                  {formatCurrency(cart.itemsPrice)}
+                </span>
+              </div>
+              <Button
+                className="w-full"
+                disabled={isPending}
+                onClick={() => {
+                  startTransition(() => router.push("/shipping-address"));
+                }}
+              >
+                {isPending ? (
+                  <Loader className="w-4 h-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )} Proceed to checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
