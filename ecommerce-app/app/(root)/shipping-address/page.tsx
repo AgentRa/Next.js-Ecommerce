5c698394a69bab8b0c492ac465798eeb63d65864
agentRa <bubonik.san1993@gmail.com>
2025-01-08 11:29:57 +0100

    Feat: add checkout steps component

--- ecommerce-app/app/(root)/shipping-address/page.tsx
+++ ecommerce-app/app/(root)/shipping-address/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
 import ShippingAddressForm from "@/app/(root)/shipping-address/shipping-address-form";
+import CheckoutSteps from "@/components/shared/checkout-steps";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
@@ -23,6 +24,7 @@ export default async function AddressPage({}: Props) {
 
   return (
     <>
+      <CheckoutSteps current={1}/>
       <ShippingAddressForm address={user.address as ShippingAddress} />
     </>
   );
